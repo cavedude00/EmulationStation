f9df3e2050787d13e2e4b4fb93ce3ec018f955b4
Steven Selph <steven.selph@gmail.com>
2015-01-19 17:40:24 -0500

    Create a GamesDB scraper using hashing to fetch by ID.
    Conflicts:
    	es-app/CMakeLists.txt
    	es-app/src/scrapers/Scraper.cpp

--- es-app/src/scrapers/Scraper.cpp
+++ es-app/src/scrapers/Scraper.cpp
@@ -8,10 +8,16 @@
 #include "GamesDBScraper.h"
 #include "GamesDBListScraper.h"
 #include "TheArchiveScraper.h"
+#include "GamesDBScraper.h"
+#include "GamesDBShaScraper.h"
+#include "TheArchiveScraper.h"
 
 const std::map<std::string, generate_scraper_requests_func> scraper_request_funcs = boost::assign::map_list_of
 	("TheGamesDB", &thegamesdb_generate_scraper_requests)
 	("TheGamesDBList", &thegamesdblist_generate_scraper_requests)
+	("TheArchive", &thearchive_generate_scraper_requests);
+	("TheGamesDB", &thegamesdb_generate_scraper_requests)
+	("TheGamesDB w/SHA1", &thegamesdbsha_generate_scraper_requests)
 	("TheArchive", &thearchive_generate_scraper_requests);
 
 std::unique_ptr<ScraperSearchHandle> startScraperSearch(const ScraperSearchParams& params)
