a554f3209407245d6d68ffc8140d386bfe007ffd
Ludwig <cavedude@peqtgc.com>
2015-08-22 03:27:54 -0700

    Cleanup.

--- es-app/src/scrapers/Scraper.cpp
+++ es-app/src/scrapers/Scraper.cpp
@@ -7,16 +7,12 @@
 
 #include "GamesDBScraper.h"
 #include "GamesDBListScraper.h"
-#include "TheArchiveScraper.h"
-#include "GamesDBScraper.h"
 #include "GamesDBShaScraper.h"
 #include "TheArchiveScraper.h"
 
 const std::map<std::string, generate_scraper_requests_func> scraper_request_funcs = boost::assign::map_list_of
 	("TheGamesDB", &thegamesdb_generate_scraper_requests)
 	("TheGamesDBList", &thegamesdblist_generate_scraper_requests)
-	("TheArchive", &thearchive_generate_scraper_requests);
-	("TheGamesDB", &thegamesdb_generate_scraper_requests)
 	("TheGamesDB w/SHA1", &thegamesdbsha_generate_scraper_requests)
 	("TheArchive", &thearchive_generate_scraper_requests);
 
